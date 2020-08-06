fabd199f777cd63eede2ac7ef75bc91c9b773b9c
Daryl Galvez <daryl.kevin.24@gmail.com>
2020-08-06 20:51:12 +0800

    Feat: Assign domain names and clicks to variables

--- domain_counters.py
+++ domain_counters.py
@@ -9,3 +9,10 @@ def by_value(item):
     return item[1]
 
 # Main function for sorting out domain hits
+def domain_counter(file):
+    with open(file) as csvfile:
+        domain_file = csv.reader(csvfile, delimiter=',')
+        # Defines domain names and click count from csv
+        for row in domain_file:
+            full_domain = row[1]
+            count = row[0]
